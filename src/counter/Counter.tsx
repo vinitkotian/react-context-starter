d90e7f12f035282213775c02da2cb9aae748529f
Vinit-Kotian <Vinit.Kotian@kas-services.com>
2025-04-07 23:31:06 +0530

    Vinit | Style the counter module

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import  styles from  './Counter.module.scss';
+import {useState} from 'react';
+import styles from './Counter.module.scss';
 import * as React from "react";
 
 
-const Counter: React.FC = ()=> {
+const Counter: React.FC = () => {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.counter}>
-            <h1>Counter</h1>
-            <p className="count">{count}</p>
+            <p className={styles.text}>Counter</p>
+            <p className={styles.text}>
+                {count}
+            </p>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
